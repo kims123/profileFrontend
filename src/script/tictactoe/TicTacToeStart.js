845a02c6905da307c8c0655d2283efa7b16fd249
kim <fletn3000@hotmail.com>
2022-09-11 17:42:01 +0200

    Restart button

--- src/script/tictactoe/TicTacToeStart.js
+++ src/script/tictactoe/TicTacToeStart.js
@@ -5,18 +5,24 @@ export function TicTacToeStart() {
 
     const [playerButtons, setPlayerButtons] = useState([])
     const [computerButtons, setComputerButtons] = useState([])
+    const [gameOver, setGameOver] = useState(false)
 
     useEffect(() => {
-        if (playerWon()) {
+        console.log("Player buttons: " + playerButtons)
+        console.log("Comput buttons: " + computerButtons)
+        if (playerButtons.length !== 0 && playerButtons.length + computerButtons.length !== 9 && playerButtons.length > computerButtons.length) {
+            computerTurn()
+        } else if (playerWon()) {
+            setGameOver(true)
             document.getElementById("tictactoe-status-text").innerText = "The winner is player!"
         } else if (computerWon()) {
+            setGameOver(true)
             document.getElementById("tictactoe-status-text").innerText = "The winner is computer!"
         } else if (playerButtons.length + computerButtons.length === 9) {
+            setGameOver(true)
             document.getElementById("tictactoe-status-text").innerText = "It is a draw!"
-        } else if (playerButtons.length !== 0 && playerButtons.length + computerButtons.length !== 9) {
-            computerTurn()
         }
-    }, [playerButtons]);
+    }, [playerButtons, computerButtons]);
 
     function wincondition1(list) {
         const winValues = [1, 2, 3]
@@ -75,6 +81,7 @@ export function TicTacToeStart() {
     }
 
     function playerWon() {
+        console.log("Player won? : " + playerButtons)
         if (wincondition1(playerButtons)) {
             return true;
         }
@@ -102,6 +109,7 @@ export function TicTacToeStart() {
     }
 
     function computerWon() {
+        console.log("Computer won? : " + computerButtons)
         if (wincondition1(computerButtons)) {
             return true;
         }
@@ -317,18 +325,22 @@ export function TicTacToeStart() {
         setPlayerButtons(playerButtonsValues)
     }
 
+    function restartGame() {
+        setPlayerButtons([])
+        setComputerButtons([])
+        setGameOver(false)
+        document.getElementById("tictactoe-status-text").innerText = "Who will be the winner d(◉◡◉)b"
+    }
+
     const computerTurn = () => {
-        console.log("Computer - Thinking...")
+        console.log("Computer making his turn")
         while (true) {
             let buttonNumber = Math.floor(Math.random() * (9) + 1);
-            console.log("Computer - Can I press " + buttonNumber + "?")
-            console.log(computerButtons)
             switch (buttonNumber) {
                 case 1: {
                     if (button1pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(1)
                     return
                 }
@@ -336,7 +348,6 @@ export function TicTacToeStart() {
                     if (button2pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(2)
                     return
                 }
@@ -344,7 +355,6 @@ export function TicTacToeStart() {
                     if (button3pressed) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(3)
                     return
                 }
@@ -352,7 +362,6 @@ export function TicTacToeStart() {
                     if (button4pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(4)
                     return
                 }
@@ -360,7 +369,6 @@ export function TicTacToeStart() {
                     if (button5pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(5)
                     return
                 }
@@ -368,7 +376,6 @@ export function TicTacToeStart() {
                     if (button6pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(6)
                     return
                 }
@@ -376,7 +383,6 @@ export function TicTacToeStart() {
                     if (button7pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(7)
                     return
                 }
@@ -384,7 +390,6 @@ export function TicTacToeStart() {
                     if (button8pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(8)
                     return
                 }
@@ -392,7 +397,6 @@ export function TicTacToeStart() {
                     if (button9pressed()) {
                         continue
                     }
-                    console.log("Computer - Yes I can. Pressing " + buttonNumber + ".")
                     updateComputerList(9)
                     return
                 }
@@ -404,29 +408,31 @@ export function TicTacToeStart() {
     }
 
     return (
-        <div>
+        <div style={{display: "inline"}}>
             <div className="tictactoe-start-container">
-                <button className="tictactoe-start-grid-item" value="1"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="1"
                         onClick={() => pressButton1()}>{button1Value()}</button>
-                <button className="tictactoe-start-grid-item" value="2"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="2"
                         onClick={() => pressButton2()}>{button2Value()}</button>
-                <button className="tictactoe-start-grid-item" value="3"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="3"
                         onClick={() => pressButton3()}>{button3Value()}</button>
-                <button className="tictactoe-start-grid-item" value="4"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="4"
                         onClick={() => pressButton4()}>{button4Value()}</button>
-                <button className="tictactoe-start-grid-item" value="5"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="5"
                         onClick={() => pressButton5()}>{button5Value()}</button>
-                <button className="tictactoe-start-grid-item" value="6"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="6"
                         onClick={() => pressButton6()}>{button6Value()}</button>
-                <button className="tictactoe-start-grid-item" value="7"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="7"
                         onClick={() => pressButton7()}>{button7Value()}</button>
-                <button className="tictactoe-start-grid-item" value="8"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="8"
                         onClick={() => pressButton8()}>{button8Value()}</button>
-                <button className="tictactoe-start-grid-item" value="9"
+                <button disabled={gameOver} className="tictactoe-start-grid-item" value="9"
                         onClick={() => pressButton9()}>{button9Value()}</button>
             </div>
             <p id="tictactoe-status-text" style={{textAlign: "center"}}>Who will be the winner d(◉◡◉)b</p>
-
+            <div style={{textAlign: "center"}}>
+                <button onClick={restartGame}>Restart</button>
+            </div>
         </div>
     )
 }
