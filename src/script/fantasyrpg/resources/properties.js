a953782c6830879f8991cd5199a65029d07557fc
kim <fletn3000@hotmail.com>
2023-02-22 07:48:49 +0100

    Oppdaterer properties med url til aws

--- src/script/fantasyrpg/resources/properties.js
+++ src/script/fantasyrpg/resources/properties.js
@@ -1,4 +1,6 @@
 export const properties = {
-    hostUrl: "http://localhost:8081",
+    // hostUrl: "http://localhost:8081",
+    hostUrl: "http://ec2-18-184-29-664.eu-central-1.compute.amazonaws.com",
+
     // hostUrl: "https://profile-application-1.herokuapp.com"
 }
--- src/script/todolist/resources/properties.js
+++ src/script/todolist/resources/properties.js
@@ -1,4 +1,4 @@
 export const properties = {
-    hostUrl: "http://localhost:8083",
-    // hostUrl: "https://profile-application-1.herokuapp.com"
+    // hostUrl: "http://localhost:8083",
+    hostUrl: "http://ec2-18-184-29-66.eu-central-1.compute.amazonaws.com:8080",
 }
