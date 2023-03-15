da6068bc6aa2741e886873892a8d23b15764d220
kim <fletn3000@hotmail.com>
2023-03-15 06:53:58 +0100

    Endrer url til aws

--- src/script/profile/resources/properties-profile.js
+++ src/script/profile/resources/properties-profile.js
@@ -1,6 +1,6 @@
 export const propertiesProfile = {
     // hostUrl: "http://localhost:8080",
-    hostUrlProfile: "http://ec2-18-195-125-184.eu-central-1.compute.amazonaws.com:8080",
+    hostUrlProfile: "http://ec2-3-125-33-179.eu-central-1.compute.amazonaws.com:8080",
 
-    hostWeatherAppUrl: "http://localhost:8082"
+    hostWeatherAppUrl: "http://ec2-18-196-224-110.eu-central-1.compute.amazonaws.com:8080"
 }
--- src/script/todolist/resources/properties-todo.js
+++ src/script/todolist/resources/properties-todo.js
@@ -1,4 +1,4 @@
 export const propertiesTodo = {
-    hostUrl: "http://localhost:8083",
-    // hostUrl: "https://ec2-18-184-29-66.eu-central-1.compute.amazonaws.com:8080",
+    // hostUrl: "http://localhost:8083",
+    hostUrl: "http://ec2-18-196-224-110.eu-central-1.compute.amazonaws.com:8080",
 }
