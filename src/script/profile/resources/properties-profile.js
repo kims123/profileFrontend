654a13c501043260ddbcddbe8e2f7d6b8809890a
Kims <fletn3000@hotmail.com>
2025-10-14 20:55:58 +0200

    Updates

--- src/script/profile/resources/properties-profile.js
+++
@@ -1,6 +0,0 @@
-export const propertiesProfile = {
-    // hostUrl: "http://localhost:8080",
-    hostUrlProfile: "http://ec2-18-195-125-184.eu-central-1.compute.amazonaws.com:8080",
-
-    hostWeatherAppUrl: "http://localhost:8082"
-}
