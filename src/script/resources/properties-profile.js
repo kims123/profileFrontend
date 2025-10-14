4adbdd27afb171979428849c2e7c3ef28570a9a9
Kims <fletn3000@hotmail.com>
2025-10-14 21:13:10 +0200

    Endrer hosturl

--- src/script/resources/properties-profile.js
+++ src/script/resources/properties-profile.js
@@ -1,6 +1,7 @@
 export const propertiesProfile = {
     hostUrlProfileLocalhost: "http://localhost:8080",
-    hostUrlProfile: "http://ec2-54-93-66-189.eu-central-1.compute.amazonaws.com:8080",
+    // hostUrlProfile: "http://ec2-54-93-66-189.eu-central-1.compute.amazonaws.com:8080",
+    hostUrlProfile: "https://profilebackend-7mfm.onrender.com",
 
     hostWeatherAppUrlLocalhost: "http://localhost:8080",
     hostWeatherAppUrl: "http://ec2-3-64-255-240.eu-central-1.compute.amazonaws.com:8080",
