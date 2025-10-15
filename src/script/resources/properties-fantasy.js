e05739ca976ae29ccbf71fd50667594cdab1dcb4
Kims <fletn3000@hotmail.com>
2025-10-15 23:25:09 +0200

    Endrer url fantasy rpg

--- src/script/resources/properties-fantasy.js
+++ src/script/resources/properties-fantasy.js
@@ -1,4 +1,5 @@
 export const propertiesFantasy = {
     hostUrlFantasyRpgLocalhost: "http://localhost:8081",
-    hostUrlFantasyRpg: "http://ec2-3-64-255-240.eu-central-1.compute.amazonaws.com:8080",
+    // hostUrlFantasyRpg: "http://ec2-3-64-255-240.eu-central-1.compute.amazonaws.com:8080",
+    hostUrlFantasyRpg: "https://fantasyrpg.onrender.com",
 }
